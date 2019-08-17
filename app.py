d6e0bc32aad9c43f419df1a7a1f889135e2021d6
Matt Bidewell <matthewbidewell@gmail.com>
2019-08-17 17:17:14 +0100

    wip
    Co-authored-by: Luke Williamson <lukewilliamson1995@gmail.com>

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 # Weather
 
 
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+from urllib.parse import quote
+from json import dumps
+
 import requests
 
-default_lang="en-US"
+base_url="http://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=(c)&culture=(en-us)&weasearchstr="
+
 default_timeout=10000
-default_degree_type="c"
-base_url="http://weather.service.msn.com/find.aspx"
 
-example_url="http://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=(c)&culture=(en-us)&weasearchstr=borehamwood"
 
-def get_request(URL, PARAMS):
-    print("sending request to" + url)
-    response = requests.get(url = URL, params = PARAMS)
-    data = response.json()
-    return handle_response(data)
 
+def run(LOC): 
+    json_response = get_request(LOC)
+    weather_obj = handle_response(json_response)
+    return print(weather_obj)
+
+def get_request(LOC):
+    print("sending request to: " + LOC)
+    encoded_loc = quote(LOC)
+    print(base_url+encoded_loc)
+    response = requests.get(base_url+encoded_loc)
+    
+    if(response.status_code == 200) :
+        unformatted_data = response.text
+        json_data = bf.data(fromstring(unformatted_data))
+        return json_data
+    
 def handle_response(data):
-    #todoS
+    print(data)
+    return "nothing"
 
-
-
-
-#findUrl + '?src=outlook&weadegreetype=' + (''+degreeType) + '&culture=' + (''+lang) + '&weasearchstr=' + search;
+run("London")
